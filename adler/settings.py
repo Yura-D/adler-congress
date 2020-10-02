9fa7b2d63c131c2613fda901c9b818722d72171e
Yuriy Dyachuk <dyachuk.yu@gmail.com>
2020-10-02 23:13:35 +0300

    Added bootstrap basic file and configured it in settings

--- adler/settings.py
+++ adler/settings.py
@@ -14,7 +14,7 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
+PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'adler.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['adler/templates/'],
+        'DIRS': [os.path.join((PROJECT_DIR), 'templates/')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -122,9 +122,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
 STATIC_ROOT = os.path.join(BASE_DIR, 'static_collected')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-MEDIA_URL = '/media/'
--- adler/website/views.py
+++ adler/website/views.py
@@ -5,9 +5,13 @@ from django.template import loader
 def index(request):
     some_list = [f'Some good text {i}' for i in range(100)]
     context = {'some_list': some_list}
-    return render(request, 'index.html', context)
+    return render(request, 'home-page.html', context)
 
 
 def program(request):
-    return render(request, 'program.html')
+    return render(request, 'post-page.html')
+
+
+def category(request):
+    return render(request, 'category-page.html')
 
